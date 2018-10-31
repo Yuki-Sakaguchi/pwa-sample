bb91fc0fd73aa27e808c7789176e1d07f9bd849b
Yuki-Sakaguchi <sakaguchi.ba55.0ne@gmail.com>
2018-11-01 00:27:55 +0900

    [modify] push

--- js/sw.js
+++ js/sw.js
@@ -1,31 +1,23 @@
-
-// ServiceWorker処理：https://developers.google.com/web/fundamentals/primers/service-workers/?hl=ja
-
-// キャッシュ名とキャッシュファイルの指定
-var CACHE_NAME = 'pwa-sample-caches';
-var urlsToCache = [
-    '/',
-    '/css/style.css',
-];
-
-// インストール処理
-self.addEventListener('install', function(event) {
-    event.waitUntil(
-        caches
-            .open(CACHE_NAME)
-            .then(function(cache) {
-                return cache.addAll(urlsToCache);
-            })
-    );
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    new Response('サービスワーカーが動いています！')
+  );
 });
 
-// リソースフェッチ時のキャッシュロード処理
-self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        caches
-            .match(event.request)
-            .then(function(response) {
-                return response ? response : fetch(event.request);
-            })
-    );
+self.addEventListener('push', function (event) {
+  console.log('Received a push message', event);
+  var title = "プッシュ通知です！";
+  var body = "プッシュ通知はこのようにして送られるのです";
+
+  event.waitUntil(
+      self.registration.showNotification(title, {
+          body: body,
+          icon: 'http://free-images.gatag.net/images/201108090000.jpg',
+          tag: 'push-notification-tag'
+      })
+  );
 });
+self.addEventListener('notificationclick', function (event) {
+  event.notification.close();
+  clients.openWindow("/");
+}, false);
